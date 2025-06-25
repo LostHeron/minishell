1d9dc321296c137d255ffe2e289c3b62c9c4aa36
Corentin Viel <cviel@z2r4p3.42lyon.fr>
2025-06-25 17:12:41 +0200

    [FIX] fixed variable expansion for 0

--- src/expand/expand_variables.c
+++ src/expand/expand_variables.c
@@ -15,8 +15,9 @@
 #include "ft_vectors.h"
 #include "ft_char.h"
 #include "ft_string.h"
+#include "ft_standard.h"
 
-static int	expand_here(t_exp *p_exp_part, int *p_ind, t_minishell mini);
+static int	expand_here(t_exp *p_exp_part, int p_ind, t_minishell mini);
 
 int	expand_variables(t_vector splitted, t_minishell mini)
 {
@@ -34,7 +35,7 @@ int	expand_variables(t_vector splitted, t_minishell mini)
 			{
 				if (((t_exp *)splitted.data)[i].content[j] == '$')
 				{
-					ret = expand_here(&((t_exp *)splitted.data)[i], &j, mini);
+					ret = expand_here(&((t_exp *)splitted.data)[i], j, mini);
 					if (ret != 0)
 						return (ret);
 				}
@@ -60,7 +61,7 @@ static t_env	*get_env(char *str, int	ind, int len_key, t_minishell mini)
 	return (lst_i->content);
 }
 
-static int	replace_key_value(t_exp *p_exp_part, int *p_ind, int len_key, t_env *p_env)
+static int	replace_key_value(t_exp *p_exp_part, int p_ind, int len_key, t_env *p_env)
 {
 	char	*replace;
 	int		len_replace;
@@ -75,27 +76,56 @@ static int	replace_key_value(t_exp *p_exp_part, int *p_ind, int len_key, t_env *
 	if (replace == NULL)
 		return (ERROR_MALLOC);
 	*replace = '\0';
-	ft_strncat(replace, p_exp_part->content, *p_ind);
+	ft_strncat(replace, p_exp_part->content, p_ind);
 	if (p_env != NULL)
 		ft_strlcat(replace, p_env->value, len_replace);
-	ft_strlcat(replace, p_exp_part->content + *p_ind + len_key + 1, len_replace);
+	ft_strlcat(replace, p_exp_part->content + p_ind + len_key + 1, len_replace);
 	free(p_exp_part->content);
 	p_exp_part->content = replace;
 	return (0);
 }
 
-static int	expand_here(t_exp *p_exp_part, int *p_ind, t_minishell mini)
+static int	replace_err_code(t_exp *p_exp_part, int p_ind, t_minishell mini)
+{
+	char	*replace;
+	char	*err_code;
+	int		len_replace;
+	int		len_code;
+
+	err_code = ft_itoa(mini.last_error_code);
+	if (err_code == NULL)
+		return (ERROR_MALLOC);
+	len_code = ft_strlen(err_code);
+	len_replace = ft_strlen(p_exp_part->content) - 2 + len_code + 1;
+	replace = malloc(sizeof(char) * len_replace);
+	if (replace == NULL)
+	{
+		free(err_code);
+		return (ERROR_MALLOC);
+	}
+	*replace = '\0';
+	ft_strncat(replace, p_exp_part->content, p_ind);
+	ft_strlcat(replace, err_code, len_replace);
+	ft_strlcat(replace, p_exp_part->content + p_ind + 2, len_replace);
+	free(p_exp_part->content);
+	p_exp_part->content = replace;
+	return (0);	
+}
+
+static int	expand_here(t_exp *p_exp_part, int p_ind, t_minishell mini)
 {
 	t_env	*p_env;
 	int		len_key;
 
-	if (ft_isalpha(p_exp_part->content[*p_ind + 1]) == 0 \
-		&& p_exp_part->content[*p_ind + 1] != '_' )
+	if (p_exp_part->content[p_ind + 1] == '?')
+		return (replace_err_code(p_exp_part, p_ind, mini));
+	if (ft_isalpha(p_exp_part->content[p_ind + 1]) == 0 \
+		&& p_exp_part->content[p_ind + 1] != '_' )
 		return (0);
 	len_key = 1;
-	while (ft_isalnum(p_exp_part->content[*p_ind + len_key + 1]) != 0 \
-			|| p_exp_part->content[*p_ind + len_key + 1] == '_' )
+	while (ft_isalnum(p_exp_part->content[p_ind + len_key + 1]) != 0 \
+			|| p_exp_part->content[p_ind + len_key + 1] == '_' )
 		len_key++;
-	p_env = get_env(p_exp_part->content, *p_ind + 1, len_key, mini);
+	p_env = get_env(p_exp_part->content, p_ind + 1, len_key, mini);
 	return (replace_key_value(p_exp_part, p_ind, len_key, p_env));
 }
