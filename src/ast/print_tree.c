36c578fe88c0ede6fc4a261552e6ee9a86ac9d57
Corentin Viel <cviel@z1r4p2.42lyon.fr>
2025-07-22 13:13:13 +0200

    [ADD][MOD] normed everything for printing ast in case we ever use this function

--- src/ast/print_tree.c
+++ src/ast/print_tree.c
@@ -13,103 +13,75 @@
 #include <stdio.h>
 #include "ast.h"
 
-char	*print_type(t_type type)
-{
-	if (type == SUBSHELL)
-		return ("SUBSHELL");
-	if (type == COMMAND)
-		return ("COMMAND");
-	if (type == PIPE)
-		return ("PIPE");
-	if (type == AND)
-		return ("AND");
-	if (type == OR)
-		return ("OR");
-	if (type == BACKGROUND)
-		return ("BACKGROUND");
-	if (type == SEQUENCE)
-		return ("SEQUENCE");
-	return ("OTHER");
-}
-
-char	*print_redir(t_dir dir)
-{
-	if (dir == IN)
-		return ("IN");
-	if (dir == OUT)
-		return ("OUT");
-	if (dir == APPEND)
-		return ("APPEND");
-	if (dir == HEREDOC)
-		return ("HEREDOC");
-	return ("NOT_DIR");
-}
+static void	print_tabs(size_t depth);
+static void	print_content(t_vector content);
+static void	print_all_redir(t_vector dir_args);
 
 void	print_tree(t_ast *root, size_t depth)
 {
-	size_t	i;
-
 	if (root == NULL)
 		return ;
-	i = 0;
+	print_tabs(depth);
+	printf("%s", print_type(root->type));
 	if (root->type == COMMAND)
 	{
-		i = 0;
-		while (i < depth)
-		{
-			printf("\t");
-			i++;
-		}
-		printf("COMMAND : args = '");
-		i = 0;
-		while (i < root->arguments.com_args.content.size - 1)
-		{
-			printf("%s", ((char **)root->arguments.com_args.content.data)[i]);
-			if (i + 1 < root->arguments.com_args.content.size - 1)
-				printf(" ");
-			i++;
-		}
-		printf("'");
-		i = 0;
-		while (i < root->arguments.com_args.dir_args.size)
-		{
-			printf(" REDIR %lu : %s %s", i + 1, print_redir(((t_dirargs *)root->arguments.com_args.dir_args.data)[i].dir), ((t_dirargs *)root->arguments.com_args.dir_args.data)[i].filename);
-			if (i + 1 < root->arguments.com_args.dir_args.size)
-				printf(" ");
-			i++;
-		}
-		printf("\n");
+		print_content(root->arguments.com_args.content);
+		print_all_redir(root->arguments.com_args.dir_args);
 	}
 	else if (root->type == SUBSHELL)
 	{
-		i = 0;
-		while (i < depth)
-		{
-			printf("\t");
-			i++;
-		}
-		printf("SUBSHELL");
-		i = 0;
-		while (i < root->arguments.sub_args.dir_args.size)
-		{
-			printf(" REDIR %lu : %s %s", i + 1, print_redir(((t_dirargs *)root->arguments.sub_args.dir_args.data)[i].dir), ((t_dirargs *)root->arguments.sub_args.dir_args.data)[i].filename);
-			if (i + 1 < root->arguments.sub_args.dir_args.size)
-				printf(" ");
-			i++;
-		}
-		printf("\n");
+		print_all_redir(root->arguments.sub_args.dir_args);
 		print_tree(root->arguments.sub_args.sub, depth + 1);
 	}
 	else
 	{
-		i = 0;
-		while (i < depth)
-		{
-			printf("\t");
-			i++;
-		}
-		printf("%s\n", print_type(root->type));
+		printf("\n");
 		print_tree(root->arguments.op_args.left, depth + 1);
 		print_tree(root->arguments.op_args.right, depth + 1);
 	}
+}
+
+static void	print_tabs(size_t depth)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < depth)
+	{
+		printf("\t");
+		i++;
+	}
+}
+
+static void	print_content(t_vector content)
+{
+	size_t	i;
+
+	printf(" : args = '");
+	i = 0;
+	while (i < content.size - 1)
+	{
+		printf("%s", ((char **)content.data)[i]);
+		if (i + 1 < content.size - 1)
+			printf(" ");
+		i++;
+	}
+	printf("'");
+}
+
+static void	print_all_redir(t_vector dir_args)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < dir_args.size)
+	{
+		printf(" REDIR %lu : %s %s", i + 1,
+			print_redir(((t_dirargs *)dir_args.data)[i].dir),
+			((t_dirargs *)dir_args.data)[i].filename);
+		if (i + 1 < dir_args.size)
+			printf(" ");
+		i++;
+	}
+	printf("\n");
 }
